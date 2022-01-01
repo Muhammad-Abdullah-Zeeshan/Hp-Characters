f68fafda23ccebec806cbf32b9bcef781006d4b8
Muhammad-Abdullah-Zeeshan <94804711+Muhammad-Abdullah-Zeeshan@users.noreply.github.com>
2022-01-01 18:52:32 +0500

    Added some features for the display of characters
    10 Characters would show up automatically when the page loads and more can be loaded with load More button. Same for when we search for some char, only 10 shows up at first and more can be loaded through load more btn.

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
 const url = 'http://hp-api.herokuapp.com/api/characters';
 const searchBtn = document.querySelector('.submit');
 const inputContainer = document.getElementById('search');
+var childDiv =document.querySelector('.childContainer');
 var data = [];
 var filteredData = [];
 var term;
 var total = 0;
+var displayed = 0;
+
+// Fetching the Data through API
 
 async function fetchData() {
     const response = await fetch(url);
     data = await response.json();
-    await console.log(data);
+    displayDataOnHome(data);
 
 }
 
+//Diaplays Characters before search
+//Displays Characters in order (Ascending Order)
+
+function displayDataOnHome(arr) {
+    for (let i = displayed ; i < displayed + 10; i++) {
+        
+        if (typeof arr[i] === 'undefined') {
+            document.querySelector('.loadBtn').remove();
+        }
+
+        else {
+            var div = document.createElement('div');
+            div.setAttribute('class', 'characterCard');
+            document.querySelector('.childContainer').appendChild(div);
+
+            var img = document.createElement('img');
+            img.src = arr[i].image;
+            img.setAttribute('alt', arr[i].name + "'s image");
+            div.appendChild(img);
+        }
+
+    }
+
+    displayed += 10;
+
+    var loadBtn = `<button class='loadBtn'>
+            Load More
+        </button>`;
+    document.querySelector('.mainContainer').innerHTML += loadBtn;
+
+    document.querySelector('.loadBtn').addEventListener('click', ()=> {
+        document.querySelector('.loadBtn').remove();
+        displayDataOnHome(arr);
+    })
+    
+}
+
+// Search Functionality
+// Returns all characters with search term containing in their Names...
+
 searchBtn.addEventListener('click', () => {
+    document.querySelector('.childContainer').innerHTML = '';
+    if (document.querySelector('.loadBtn') != null) {
+        document.querySelector('.loadBtn').remove();
+    }
+
     if (inputContainer.value == '') {
         
     }
@@ -37,20 +86,10 @@ function returnFilteredData() {
     data.forEach((person) => {
         
         if (person.name.toLowerCase().includes(term)) {
-            console.log(person.name);
             filteredData.push(person);
             total += 1;
 
             document.querySelector('.resultNum').innerHTML = 'Results: ' + total;
-
-            var div = document.createElement('div');
-            div.setAttribute('class', 'characterCard');
-            document.querySelector('.childContainer').appendChild(div);
-            
-            var img = document.createElement('img');
-            img.src = person.image;
-            img.setAttribute('alt', person.name + "'s image");
-            div.appendChild(img);
         }
 
         else{
@@ -58,7 +97,11 @@ function returnFilteredData() {
         }
     })
 
-    console.log(total);
-}
+    //Displays the search results with load more feature!
 
+    displayed = 0;
+    displayDataOnHome(filteredData);
+
+}
+//Fetches the data from API
 fetchData();
